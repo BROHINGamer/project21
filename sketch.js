37777cd204258987676338c22cb13543b146e999
BROHINGamer <71891588+BROHINGamer@users.noreply.github.com>
2020-09-29 20:54:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var bullet,wall,thickness,speed,mass;
+function setup() {
+  createCanvas(2000,400);
+  bullet=createSprite(50, 200, 10, 10);
+  wall=createSprite(800,200,30,60);
+  speed=random(223,321);
+  mass=random(30,52);
+  thickness=random(22,83);
+}
+
+function draw() {
+  background(255,255,255); 
+  bullet.velocityX=speed; 
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage =0.5*mass*speed*speed/(thickness*thickness*thickness);
+    if(damage>10){
+      bullet.shapeColor=color(255,0,0);
+    }
+    if(damage<10){
+      bullet.shapeColor=color(0,255,0);
+    }
+    
+  }
+  drawSprites();
+}
+function hasCollided(Lbullet,Lwall){
+  bulletRightEdge=Lbullet.x+Lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
